89e1755b1489d26a3eb63d0e1afc2881e384b1ff
TommyM <tommy@sencha.com>
2012-06-07 05:40:11 +0200

    Added the toolbar at the top and added some custom icons and styling

--- app/view/Main.js
+++ app/view/Main.js
@@ -2,9 +2,75 @@ Ext.define("Pandora.view.Main", {
     extend: 'Ext.Container',
     config: {
         items: [{
-            xtype: 'toolbar',
             docked: 'top',
-            title: 'Top Toolbar'
+            xtype: 'toolbar',
+            height: 90,
+            items: [
+                {
+                    xtype: 'selectfield',
+                    placeHolder: 'Search station...',
+                    displayField: 'name',
+                    valueField: 'id',
+                    width: 180
+                },
+                {
+                    xtype: 'container',
+                    flex: 1,
+                    layout: {
+                        type: 'vbox',
+                        align: 'center'
+                    },
+                    items: [
+                        {
+                            xtype: 'container',
+                            defaults: {
+                                xtype: 'button',
+                                ui: 'plain',
+                                iconMask: true
+                            },
+                            layout: 'hbox',
+                            items: [{
+                                iconCls: 'star',
+                                action: 'vote-up'
+                            }, {
+                                iconCls: 'flag',
+                                action: 'vote-down'
+                            }, {
+                                iconCls: 'pause',
+                                action: 'pause'
+                            }, {
+                                iconCls: 'fforward',
+                                action: 'skip'
+                            }]
+                        },
+                        {
+                            xtype: 'container',
+                            layout: {
+                                type: 'hbox',
+                                align: 'center'
+                            },
+                            items: [{
+                                xtype: 'component',
+                                html: '2:00'
+                            }, {
+                                xtype: 'slider',
+                                value: 50,
+                                width: 200,
+                                minValue: 0,
+                                maxValue: 100
+                            }, {
+                                xtype: 'component',
+                                html: '4:00'
+                            }]
+                        }
+                    ]
+                },
+                {
+                    xtype: 'component',
+                    cls: 'app-logo',
+                    html: 'Sencha Radio'
+                }
+            ]
         }, {
             width: 250,
             docked: 'left',
