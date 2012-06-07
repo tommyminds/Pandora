206656ae8589195e89c20eaf935943793f69af16
TommyM <tommy@sencha.com>
2012-06-07 04:50:05 +0200

    Added the list on the left side

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,49 +1,66 @@
 Ext.define("Pandora.view.Main", {
-    extend: 'Ext.tab.Panel',
-    requires: [
-        'Ext.TitleBar',
-        'Ext.Video'
-    ],
+    extend: 'Ext.Container',
     config: {
-        tabBarPosition: 'bottom',
-
-        items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
+        items: [{
+            xtype: 'toolbar',
+            docked: 'top',
+            title: 'Top Toolbar'
+        }, {
+            width: 250,
+            docked: 'left',
+            xtype: 'list',
+            grouped: true,
+            store: {
+                data: [
+                    {name: 'Black Star'},
+                    {name: 'Led Zeppelin'}
+                ],
+                grouper: function(record) {
+                    return record.get('name')[0];
+                }
+            },
+            itemTpl: '{name}',
+            items: [
+                {
                     docked: 'top',
                     xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
+                    title: 'Stations',
+                    ui: 'light'
                 },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
-            }
-        ]
+                {
+                    docked: 'bottom',
+                    xtype: 'toolbar',
+                    ui: 'light',
+                    items: [
+                        {
+                            xtype: 'button',
+                            action: 'settings',
+                            iconMask: true,
+                            iconCls: 'settings',
+                            ui: 'plain'
+                        },
+                        {
+                            xtype: 'segmentedbutton',
+                            layout: {
+                                type: 'hbox',
+                                align: 'middle'
+                            },
+                            items: [
+                                {
+                                    text: 'By Date',
+                                    action: 'filter-date'
+                                },
+                                {
+                                    text: 'ABC',
+                                    action: 'filter-name'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }, {
+            html: 'Center area takes up the rest of the space'
+        }]
     }
 });
